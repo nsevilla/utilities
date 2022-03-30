abaac74715b831eb617269b498e59ad038f506e6
Nacho Sevilla <ignacio.sevilla@ciemat.es>
2022-03-30 15:13:44 +0200

    First working version of 2ptcf with standalone python script

---
+++ compute2pt.py
@@ -0,0 +1,127 @@
+import os,sys
+import numpy as np
+import treecorr
+import fitsio
+import matplotlib
+import matplotlib.pyplot as plt
+import healpy as hp
+import healpix_util as hu
+import time
+from optparse import OptionParser
+
+def compute_2pt(data_filename,random_filename,config,binslop):
+
+    print('Loading data')
+    start = time.time()
+    datacat = treecorr.Catalog(data_filename, ra_col='RA', dec_col='DEC', ra_units='degrees', dec_units='degrees')
+    randcat = treecorr.Catalog(random_filename, ra_col='RA', dec_col='DEC', ra_units='degrees', dec_units='degrees')
+
+    print('Starting with estimation')
+    dd = treecorr.NNCorrelation(config,bin_slop=binslop) # var_method = 'bootstrap'
+    rr = treecorr.NNCorrelation(config,bin_slop=binslop)
+    dr = treecorr.NNCorrelation(config,bin_slop=binslop)
+
+    dd.process(datacat)
+    rr.process(randcat)
+    dr.process(datacat,randcat)
+
+    wtheta_counts,dwtheta_counts = dd.calculateXi(rr,dr)
+
+    th_counts = dd.meanr
+    wth_counts = wtheta_counts
+
+    np.savetxt('theta_counts_{}.csv'.format(binslop),th_counts)
+    np.savetxt('wtheta_counts_{}.csv'.format(binslop),wth_counts)
+
+    end = time.time()
+    print('Time for counts estimator with bin_slop {0} is {1} s'.format(binslop,end-start))
+
+    result = [th_counts, wth_counts]
+
+    return result
+
+def compute_2pt_pix(datadens_filename,mask_filename,config,nside=4096):
+
+## now the pixel estimator
+    #datadens_filename = workdir+'density_map.fits'
+    #mask_filename = workdir+'y3_gold_2.2.1_RING_joint_redmagic_v0.5.1_wide_maglim_v2.2_mask.fits.gz'
+    print('Loading data')
+    start = time.time()
+    density = fitsio.read(datadens_filename,ext=1)['PIXELVAL']
+    mask_pix = fitsio.read(mask_filename,ext=1)['HPIX']
+    mask_fdet = fitsio.read(mask_filename,ext=1)['FRACGOOD'] 
+
+    pixindex = np.arange(hp.nside2npix(nside))
+    pixels = pixindex[density > -1] # density field already masked
+    hpix = hu.HealPix('nest',nside)
+    ra,dec = hpix.pix2eq(pixels)
+
+    ngal = density[density > -1]
+    ngal_avg = np.average(ngal, weights = mask_fdet)
+    deltas = (ngal-ngal_avg)/ngal_avg
+
+    print('Starting with pixel estimator')
+    wtheta_pix = treecorr.KKCorrelation(config) # var_method = 'bootstrap'
+    cat = treecorr.Catalog(ra=ra,dec=dec,ra_units='degrees',dec_units='degrees', k=deltas)#, w=weights)
+    wtheta_pix.process(cat)
+    th_pix = wtheta_pix.meanr
+    wth_pix = wtheta_pix.xi
+
+    np.savetxt('theta_pix_{}.csv'.format(nside),th_pix)
+    np.savetxt('wtheta_pix_{}.csv'.format(nside),wth_pix)
+
+    end = time.time()
+    print('Time for pixel estimator with nside {0} is {1} s'.format(nside,end-time_counts))
+
+    result = [th_pix,wth_pix]
+
+    return result
+
+def plot_wth(result,config,label,outfile_label,result_comp=None,label_comp=None):
+
+    plt.xlabel('theta {}'.format(config['sep_units']))
+    plt.ylabel('theta*w(theta)')
+    plt.xscale('log')
+    plt.yscale('linear')
+    plt.xlim(config['min_sep'],config['max_sep'])
+    #plt.ylim(0.0,1.0)
+    plt.plot(result[0],result[0]*result[1],label=label)
+    plt.legend()
+    plt.savefig('thwth_{}.png'.format(outfile_label))
+
+    if result_comp is not None:
+        plt.figure()
+        plt.xscale('log')
+        plt.yscale('linear')
+        plt.plot(result[0],result_comp[1]-result[1])
+        plt.savefig('wth_residual_{}_{}.png'.format(label,label_comp))
+
+def main():
+    '''
+    Run code with options
+    '''
+    usage = "%prog [options]"
+    parser = OptionParser(usage=usage)
+
+    (options, args) = parser.parse_args()
+    parser.add_option("--data_filename",type="string",dest="data_filename",help="Data file name",default="/afs/ciemat.es/user/s/sevilla/projects/stellarlss_project/Y3MLLOZ_weighted.fits")
+    parser.add_option("--random_filename",type="string",dest="random_filename",help="Random file name",default="/afs/ciemat.es/user/s/sevilla/projects/stellarlss_project/random.fits")
+    parser.add_option("--min_sep",type="float",dest="min_sep",help="Minimum theta",default=2.0)
+    parser.add_option("--max_sep",type="float",dest="max_sep",help="Maximum theta",default=220.0)
+    parser.add_option("--nbins",type="int",dest="nbins",help="Number of bins",default=20)
+    parser.add_option("--sep_units",type="string",dest="sep_units",help="Units of theta",default="arcmin")
+    parser.add_option("--binslop",type="float",dest="binslop",help="bin_slop for treecorr",default=0.1)   
+    (options, args) = parser.parse_args()
+
+    config = {"min_sep" : options.min_sep, 
+              "max_sep" : options.max_sep, 
+              "nbins" : options.nbins, 
+              "sep_units" : options.sep_units, 
+              "verbose" : 2}
+    
+    result = compute_2pt(options.data_filename,options.random_filename,config,options.binslop)
+    #compute_2pt_pix()
+    plot_wth(result,config,label='Treecorr binslop {}'.format(options.binslop),outfile_label='treecorr_binslop_{}'.format{options.binslop})
+
+if __name__ == "__main__":
+    main()
