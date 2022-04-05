e4cde3a9f6ec1b6591a436abe0b9d5167a06ecb7
Nacho Sevilla <ignacio.sevilla@ciemat.es>
2022-04-05 11:25:49 +0200

    Fixed bugs in variable names

--- compute2pt.py
+++ compute2pt.py
@@ -9,14 +9,18 @@ import healpix_util as hu
 import time
 from optparse import OptionParser
 
-def compute_2pt(data_filename,random_filename,config,binslop):
+def compute_2pt(data_filename,random_filename,config,binslop,weights,csv_name):
 
     print('Loading data')
     start = time.time()
-    datacat = treecorr.Catalog(data_filename, ra_col='RA', dec_col='DEC', ra_units='degrees', dec_units='degrees')
+    if weights:
+        datacat = treecorr.Catalog(data_filename, ra_col='RA', dec_col='DEC', ra_units='degrees', dec_units='degrees', w_col='weight')
+    else:
+        datacat = treecorr.Catalog(data_filename, ra_col='RA', dec_col='DEC', ra_units='degrees', dec_units='degrees')
     randcat = treecorr.Catalog(random_filename, ra_col='RA', dec_col='DEC', ra_units='degrees', dec_units='degrees')
 
     print('Starting with estimation')
+    
     dd = treecorr.NNCorrelation(config,bin_slop=binslop) # var_method = 'bootstrap'
     rr = treecorr.NNCorrelation(config,bin_slop=binslop)
     dr = treecorr.NNCorrelation(config,bin_slop=binslop)
@@ -30,8 +34,8 @@ def compute_2pt(data_filename,random_filename,config,binslop):
     th_counts = dd.meanr
     wth_counts = wtheta_counts
 
-    np.savetxt('theta_counts_{}.csv'.format(binslop),th_counts)
-    np.savetxt('wtheta_counts_{}.csv'.format(binslop),wth_counts)
+    np.savetxt('theta_counts_{}.csv'.format(csv_name),th_counts)
+    np.savetxt('wtheta_counts_{}.csv'.format(csv_name),wth_counts)
 
     end = time.time()
     print('Time for counts estimator with bin_slop {0} is {1} s'.format(binslop,end-start))
@@ -77,7 +81,7 @@ def compute_2pt_pix(datadens_filename,mask_filename,config,nside=4096):
 
     return result
 
-def plot_wth(result,config,label,outfile_label,result_comp=None,label_comp=None):
+def plot_wth(result,config,label,outfile_name,result_comp=None,label_comp=None):
 
     plt.xlabel('theta {}'.format(config['sep_units']))
     plt.ylabel('theta*w(theta)')
@@ -86,15 +90,17 @@ def plot_wth(result,config,label,outfile_label,result_comp=None,label_comp=None)
     plt.xlim(config['min_sep'],config['max_sep'])
     #plt.ylim(0.0,1.0)
     plt.plot(result[0],result[0]*result[1],label=label)
-    plt.legend()
-    plt.savefig('thwth_{}.png'.format(outfile_label))
-
     if result_comp is not None:
-        plt.figure()
-        plt.xscale('log')
-        plt.yscale('linear')
-        plt.plot(result[0],result_comp[1]-result[1])
-        plt.savefig('wth_residual_{}_{}.png'.format(label,label_comp))
+        plt.plot(result_comp[0],result_comp[0]*result_comp[1],label=label_comp)
+    plt.legend()
+    plt.savefig('thwth_{}.png'.format(outfile_name))
+
+#    if result_comp is not None:
+#        plt.figure()
+#        plt.xscale('log')
+#        plt.yscale('linear')
+#        plt.plot(result[0],result_comp[1]-result[1])
+#        plt.savefig('wth_residual_{}_{}.png'.format(label,label_comp))
 
 def main():
     '''
@@ -103,14 +109,22 @@ def main():
     usage = "%prog [options]"
     parser = OptionParser(usage=usage)
 
-    (options, args) = parser.parse_args()
-    parser.add_option("--data_filename",type="string",dest="data_filename",help="Data file name",default="/afs/ciemat.es/user/s/sevilla/projects/stellarlss_project/Y3MLLOZ_weighted.fits")
+    parser.add_option("--data_filename",type="string",dest="data_filename",help="Data file name",default="/afs/ciemat.es/user/s/sevilla/projects/stellarlss_project/data/y3maglim_bin1_std34.fits")
     parser.add_option("--random_filename",type="string",dest="random_filename",help="Random file name",default="/afs/ciemat.es/user/s/sevilla/projects/stellarlss_project/random.fits")
-    parser.add_option("--min_sep",type="float",dest="min_sep",help="Minimum theta",default=2.0)
-    parser.add_option("--max_sep",type="float",dest="max_sep",help="Maximum theta",default=220.0)
+    parser.add_option("--min_sep",type="float",dest="min_sep",help="Minimum theta",default=2.5)
+    parser.add_option("--max_sep",type="float",dest="max_sep",help="Maximum theta",default=250.0)
     parser.add_option("--nbins",type="int",dest="nbins",help="Number of bins",default=20)
     parser.add_option("--sep_units",type="string",dest="sep_units",help="Units of theta",default="arcmin")
-    parser.add_option("--binslop",type="float",dest="binslop",help="bin_slop for treecorr",default=0.1)   
+    parser.add_option("--binslop",type="float",dest="binslop",help="bin_slop for treecorr",default=0.1) 
+    parser.add_option("--weights",action="store_true",dest="weights_flag",help="Read and apply weights column",default=False) 
+    parser.add_option("--plot_label",type="string",dest="plot_label",help="Label for plot in legend",default=None)
+    parser.add_option("--compare",action="store_true",dest="compare_flag",help="Make comparison with another 2pcf",default=False)
+    parser.add_option("--data_filename_comp",type="string",dest="data_filename_comp",help="Data file name to compare",default=None)
+    parser.add_option("--random_filename_comp",type="string",dest="random_filename_comp",help="Random file name to compare",default=None)
+    parser.add_option("--binslop_comp",type="float",dest="binslop_comp",help="bin_slop for treecorr for comparison test",default=0.1)
+    parser.add_option("--weights_comp",action="store_true",dest="weights_comp_flag",help="Read and apply weights column to comparison data",default=False) 
+    parser.add_option("--plot_label_comp",type="string",dest="plot_label_comp",help="Label for comparison plot in legend",default=None)
+    parser.add_option("--outfile",type="string",dest="outfile_name",help="Label for plot",default="treecorr") 
     (options, args) = parser.parse_args()
 
     config = {"min_sep" : options.min_sep, 
@@ -119,9 +133,15 @@ def main():
               "sep_units" : options.sep_units, 
               "verbose" : 2}
     
-    result = compute_2pt(options.data_filename,options.random_filename,config,options.binslop)
-    #compute_2pt_pix()
-    plot_wth(result,config,label='Treecorr binslop {}'.format(options.binslop),outfile_label='treecorr_binslop_{}'.format{options.binslop})
+    result = compute_2pt(options.data_filename,options.random_filename,config,options.binslop,options.weights_flag,"1")
+    result_comp = None
+    if options.compare_flag:
+        result_comp = compute_2pt(options.data_filename_comp,options.random_filename_comp,config,options.binslop_comp,options.weights_comp_flag,"2")
+        #result_comp = compute_2pt_pix()
+    plot_wth(result,config,
+             label='{}'.format(options.plot_label),
+             outfile_name='{}'.format(options.outfile_name),
+             result_comp=result_comp,label_comp='{}'.format(options.plot_label_comp))
 
 if __name__ == "__main__":
     main()
