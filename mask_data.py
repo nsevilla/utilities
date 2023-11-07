3bc5c1398476b4bff9b13785d09aab5cbcfbb63c
nsevilla <nsevilla@gmail.com>
2023-11-07 22:52:03 +0100

    Added healsparse option for input

--- mask_data.py
+++ mask_data.py
@@ -1,38 +1,48 @@
 #!/usr/bin/env python
 '''
-This script will mask catalogs
-Use this script to apply a healpix map to a catalog of data
+This script will mask catalogs 
+Use this script to apply a healpix map to a catalog 
 
 Author: Nacho Sevilla
-Usage: python mask_data.py
+Usage: python mask_data.py [options]
 '''
 
 import os,sys
 import numpy as np
 import healpy as hp
+import healsparse as hsp
 from astropy.table import Table
 from astropy.io import fits
 from astropy.io import ascii
 import fitsio
 from optparse import OptionParser
+import matplotlib
+from matplotlib import pyplot as plt
+matplotlib.use('Agg')
 
 nside = 4096
 
-def mask_catalog(catalog_file,mask_file,nest):
+def mask_data(data_file,mask_file,nest,healsparse):
     '''Mask input catalog'''
     print('Reading mask',mask_file)
-    #mask = fitsio.read(mask_file,ext=1)['I'].ravel()
-    hdu_mask = fits.open(mask_file)
-    mask = hdu_mask[1].data    
 
-    mask_exp = np.zeros(hp.nside2npix(nside))
-    mask_exp[mask['HPIX']] = mask['FRACGOOD']
-    if not nest:
-        mask_exp = hp.reorder(mask_exp,r2n=True)
+    if healsparse:
+        mask_exp = hsp.HealSparseMap.read(mask_file)
+    else:
+        #mask = fitsio.read(mask_file,ext=1)['I'].ravel()
+        hdu_mask = fits.open(mask_file)
+        mask = hdu_mask[1].data    
+        mask_exp = np.full(hp.nside2npix(nside),hp.UNSEEN)
+        #mask_exp[mask['HPIX']] = mask['FRACGOOD']
+        mask_exp[mask['PIXEL']] = mask['I']
+        if not nest:
+            mask_exp = hp.reorder(mask_exp,r2n=True)
         
-    print('Reading catalog',catalog_file)
-    hdu = fits.open(catalog_file)
+    print('Reading catalog',data_file)
+    hdu = fits.open(data_file)
     data = hdu[1].data   
+
+    #print(np.min(mask_exp))
 
     ra = data['RA']
     dec = data['DEC']
@@ -45,7 +55,6 @@ def mask_catalog(catalog_file,mask_file,nest):
     for counter,p in enumerate(pix):
         if counter%100000 == 0:
             print('Masked',counter,'objects') 
-    #    if ((mask_y3_a[p] > 0) and (mask_y3_b[p] == 0)):  # (mask_y1_ext[p] > 0): 
         if (mask_exp[p] > 0):
             good[counter] = True
 
@@ -54,7 +63,7 @@ def mask_catalog(catalog_file,mask_file,nest):
 
     return data[good]
     
-def write_catalog(data,out_file):
+def write_data(data,out_file):
     '''Write catalog to fits file'''
     print('Writing to file',out_file)
     hdu_masked = fits.BinTableHDU(data=data)
@@ -68,13 +77,16 @@ def main():
     usage = "%prog [options]"
     parser = OptionParser(usage=usage)
     parser.add_option("--mask",type="string",dest="mask",help="Mask file",default='/Users/nsevilla/des/data/y3y1_redmagic_comparison/y1_mask.fits.gz')
-    parser.add_option("--catalog",type="string",dest="catalog",help="Catalog file",default='/Users/nsevilla/des/data/y3y1_redmagic_comparison/y3_redmagic.fits.gz')
-    parser.add_option("--outfile",type="string",dest="out",help="Output file",default='masked.fits')
+    parser.add_option("--infile",type="string",dest="infile",help="Input data file",default='/Users/nsevilla/des/data/y3y1_redmagic_comparison/y3_redmagic.fits.gz')
+    parser.add_option("--outfile",type="string",dest="outfile",help="Output file",default='masked.fits')
     parser.add_option("--nest",action="store_true",dest="nest",help="Set mask healpix coding to NEST",default=False)
+    parser.add_option("--healsparse",action="store_true",dest="healsparse",help="Set input mask as in healsparse format",default=False)
     (options, args) = parser.parse_args()
 
-    data = mask_catalog(options.catalog,options.mask,options.nest)
-    write_catalog(data,options.out)
+    data = mask_data(options.infile,options.mask,options.nest,options.healsparse)
+
+    write_data(data,options.outfile)
+
     return 0
 
 if __name__ == "__main__":
